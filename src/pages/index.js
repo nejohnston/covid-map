3481c0156b351a7661d7e7d2638dbe5aa8f42eb2
Nicholas Johnston <nicholas.johnston94@gmail.com>
2020-05-11 18:35:10 -0700

    added new styles, svg from mapbox

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,17 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import L from 'leaflet';
 
-import './hubs.json';
-import { promiseToFlyTo, getCurrentLocation } from 'lib/map';
+import axios from 'axios';
 
 import Layout from 'components/Layout';
-import Container from 'components/Container';
 import Map from 'components/Map';
 
 const LOCATION = {
-  lat: 38.9072,
-  lng: -77.0369
+  lat: 0,
+  lng: 0,
 };
-const CENTER = [ LOCATION.lat, LOCATION.lng ];
-const DEFAULT_ZOOM = 2;
+const CENTER = [LOCATION.lat, LOCATION.lng];
+const DEFAULT_ZOOM = 3;
 
 const IndexPage = () => {
   /**
@@ -22,40 +20,95 @@ const IndexPage = () => {
    * @description Fires a callback once the page renders
    * @example Here this is and example of being used to zoom in and set a popup on load
    */
-  const { data = [] } = response;
-  const hasData = Array.isArray(data) && data.length > 0;
 
-  if (!hasData) return;
+  async function mapEffect({ leafletElement: map } = {}) {
+    let response;
 
-  const geoJson = {
-    type: 'FeatureCollection',
-    features: data.map((country = {}) => {
-      const { countryInfo = {} } = country;
-      const { lat, long: lng } = countryInfo;
-      return {
-        type: 'Feature',
-        properties: {
-          ...country
-        },
-        geometry: {
-          type: 'Point',
-          coordinates: [ lng, lat ]
+    try {
+      response = await axios.get( 'https://corona.lmao.ninja/v2/countries' );
+    } catch ( e ) {
+      console.log( `Failed to fetch countries: ${e.message}`, e );
+      return;
+    }
+
+    const { data = [] } = response;
+    const hasData = Array.isArray( data ) && data.length > 0;
+
+    if ( !hasData ) return;
+
+    const geoJson = {
+      type: 'FeatureCollection',
+      features: data.map(( country = {}) => {
+        const { countryInfo = {} } = country;
+        const { lat, long: lng } = countryInfo;
+        return {
+          type: 'Feature',
+          properties: {
+            ...country,
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [lng, lat],
+          },
+        };
+      }),
+    };
+
+    const geoJsonLayers = new L.GeoJSON( geoJson, {
+      pointToLayer: ( feature = {}, latlng ) => {
+        const { properties = {} } = feature;
+        let updatedFormatted;
+        let casesString;
+
+        const { country, updated, cases, deaths, recovered } = properties;
+
+        casesString = `${cases}`;
+
+        if ( cases > 1000 ) {
+          casesString = `${casesString.slice( 0, -3 )}k+`;
         }
-      };
-    })
-  };
 
-  async function mapEffect({ leafletElement: map } = {}) {}
+        if ( updated ) {
+          updatedFormatted = new Date( updated ).toLocaleString();
+        }
+
+        const html = `
+          <span class="icon-marker">
+            <span class="icon-marker-tooltip">
+              <h2>${country}</h2>
+              <ul>
+                <li><strong>Confirmed:</strong> ${cases}</li>
+                <li><strong>Deaths:</strong> ${deaths}</li>
+                <li><strong>Recovered:</strong> ${recovered}</li>
+                <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+              </ul>
+            </span>
+            ${casesString}
+          </span>
+        `;
+
+        return L.marker( latlng, {
+          icon: L.divIcon({
+            className: 'icon',
+            html,
+          }),
+          riseOnHover: true,
+        });
+      },
+    });
+
+    geoJsonLayers.addTo( map );
+  }
 
   const mapSettings = {
     center: CENTER,
     defaultBaseMap: 'OpenStreetMap',
     zoom: DEFAULT_ZOOM,
-    mapEffect
+    mapEffect,
   };
 
   return (
-    <Layout pageName='home'>
+    <Layout pageName="home">
       <Helmet>
         <title>Home Page</title>
       </Helmet>
