3e78a7ec123aeeb872068a7609d53cf1db530a7c
Nicholas Johnston <nicholas.johnston94@gmail.com>
2020-05-20 22:43:19 -0700

    readded header scss

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,6 @@ module.exports = {
     'gatsby-plugin-resolve-src',
     'gatsby-plugin-sass',
     'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/assets/images`,
-      },
-    },
     'gatsby-plugin-react-leaflet',
   ],
 };
