f1b305459bff5725573165c9f97beced91ea0b08
Nicholas Johnston <nicholas.johnston94@gmail.com>
2020-05-20 23:00:38 -0700

    fixed facebook icon

--- src/lib/map.js
+++ src/lib/map.js
@@ -3,26 +3,26 @@ import L from 'leaflet';
  * promiseToFlyTo
  * @description
  */
-export function promiseToFlyTo( map, { zoom, center }) {
-  return new Promise(( resolve, reject ) => {
+export function promiseToFlyTo(map, { zoom, center }) {
+  return new Promise((resolve, reject) => {
     const baseError = 'Failed to fly to area';
 
-    if ( !map.flyTo ) {
-      reject( `${baseError}: no flyTo method on map` );
+    if (!map.flyTo) {
+      reject(`${baseError}: no flyTo method on map`);
     }
 
-    if ( typeof zoom !== 'number' ) {
-      reject( `${baseError}: zoom invalid number ${zoom}` );
+    if (typeof zoom !== 'number') {
+      reject(`${baseError}: zoom invalid number ${zoom}`);
     }
 
     const mapCenter = center || map.getCenter();
     const mapZoom = zoom || map.getZoom();
 
-    map.flyTo( mapCenter, mapZoom, {
-      duration: 2,
+    map.flyTo(mapCenter, mapZoom, {
+      duration: 2
     });
 
-    map.once( 'moveend', () => {
+    map.once('moveend', () => {
       resolve();
     });
   });
@@ -34,10 +34,10 @@ export function promiseToFlyTo( map, { zoom, center }) {
  */
 
 export function getCurrentLocation() {
-  return new Promise(( resolve, reject ) => {
+  return new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(
-      ( pos ) => resolve({ lat: pos.coords.latitude, lng: pos.coords.longitude }),
-      ( err ) => reject( err )
+      (pos) => resolve({ lat: pos.coords.latitude, lng: pos.coords.longitude }),
+      (err) => reject(err)
     );
   });
 }
@@ -49,24 +49,28 @@ export function getCurrentLocation() {
 export function createTripPointsGeoJson({ locations } = {}) {
   return {
     type: 'FeatureCollection',
-    features: locations.map(({ placename, country, location = {}, image, founded, social = {} } = {}) => {
-      const { fb } = social;
-      const { lat, lng } = location;
-      return {
-        type: 'Feature',
-        properties: {
-          placename,
-          country,
-          fb,
-          founded,
-          image,
-        },
-        geometry: {
-          type: 'Point',
-          coordinates: [lng, lat],
-        },
-      };
-    }),
+    features: locations.map(
+      (
+        { placename, country, location = {}, image, founded, social = {} } = {}
+      ) => {
+        const { fb } = social;
+        const { lat, lng } = location;
+        return {
+          type: 'Feature',
+          properties: {
+            placename,
+            country,
+            fb,
+            founded,
+            image
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [ lng, lat ]
+          }
+        };
+      }
+    )
   };
 }
 
@@ -74,21 +78,21 @@ export function createTripPointsGeoJson({ locations } = {}) {
  * tripStopPointToLayer
  */
 
-export function tripStopPointToLayer( feature = {}, latlng ) {
+export function tripStopPointToLayer(feature = {}, latlng) {
   const { properties = {} } = feature;
   const { placename, country, fb, image, founded } = properties;
 
   let imageString = '';
   let text = '';
 
-  if ( image ) {
+  if (image) {
     imageString = `
       <span class="trip-stop-image" style="background-image: url(${image})">${placename}</span>
     `;
   }
 
   // Determines wether or not to display Facebook or not
-  if ( fb !== '' ) {
+  if (fb !== '') {
     text = `
     <div class="trip-stop">
       <div class="trip-stop-left-content">
@@ -99,16 +103,9 @@ export function tripStopPointToLayer( feature = {}, latlng ) {
         <div class="trip-stop-right-content">
         <div class="menu-button-wrapper">
         <a href='${fb}' id='menu-button'>
-        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" 
-        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 viewBox="0 0 29.94 29.94" style="enable-background:new 0 0 29.94 29.94;" xml:space="preserve">
+        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 29.94 29.94" style="enable-background:new 0 0 29.94 29.94;" xml:space="preserve">
 <g>
-	<path d="M27.56,0.684H2.383C1.065,0.684,0,1.748,0,3.064v23.813c0,1.312,1.065,2.379,2.383,2.379H27.56
-    c1.312,0,2.38-1.066,2.38-2.379V3.064C29.939,1.748,28.871,0.684,27.56,0.684z M20.125,9.167c-0.619
-    -0.362-1.11-0.648-1.727-0.648c-0.604,0-1.12,0.151-1.384,0.405c-0.264,0.252-0.395,0.74-0.395,1.
-    461v1.067h3.229l-0.699,2.968h-2.53v9.862h-4.056V14.42H10.67v-2.968h1.895v-1.133c0-1.193,0.143
-    -1.907,0.425-2.496c0.281-0.587,0.826-1.241,1.584-1.611c0.757-0.369,1.877-0.555,3.036-0.555c1.
-    188,0,2.116,0.396,3.254,0.715L20.125,9.167z"/>
+	<path d="M27.56,0.684H2.383C1.065,0.684,0,1.748,0,3.064v23.813c0,1.312,1.065,2.379,2.383,2.379H27.56c1.312,0,2.38-1.066,2.38-2.379V3.064C29.939,1.748,28.871,0.684,27.56,0.684z M20.125,9.167c-0.619-0.362-1.11-0.648-1.727-0.648c-0.604,0-1.12,0.151-1.384,0.405c-0.264,0.252-0.395,0.74-0.395,1.461v1.067h3.229l-0.699,2.968h-2.53v9.862h-4.056V14.42H10.67v-2.968h1.895v-1.133c0-1.193,0.143-1.907,0.425-2.496c0.281-0.587,0.826-1.241,1.584-1.611c0.757-0.369,1.877-0.555,3.036-0.555c1.188,0,2.116,0.396,3.254,0.715L20.125,9.167z"/>
 	</g>
 </svg>
  </a>
@@ -132,17 +129,17 @@ export function tripStopPointToLayer( feature = {}, latlng ) {
   }
 
   const popup = L.popup({
-    maxWidth: 400,
-  }).setContent( text );
+    maxWidth: 400
+  }).setContent(text);
 
-  const layer = L.marker( latlng, {
+  const layer = L.marker(latlng, {
     icon: L.divIcon({
       className: 'icon',
       html: `<span class="icon-trip-stop"></span>`,
-      iconSize: 20,
+      iconSize: 20
     }),
-    riseOnHover: true,
-  }).bindPopup( popup );
+    riseOnHover: true
+  }).bindPopup(popup);
 
   return layer;
 }
